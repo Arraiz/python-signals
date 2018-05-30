14864f192b29045eaa564d98249db1e191bd4e4b
arraiz <mikeldiezgarcia@gmail.com>
2018-05-30 17:08:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Signal Visualizer',
+      version='0.1',
+      description='App for Plotting signals',
+      url='https://github.com/Arraiz/python-signals',
+      author='Mikel Diez Garcia (arraiz)',
+      author_email='mdiez030@ehu.eus',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
